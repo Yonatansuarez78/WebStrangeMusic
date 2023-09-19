9a16d0d7514986f87954780ebced898e260cdee5
yonatan78 <yonatansuarez78@gmail.com>
2023-09-18 21:33:58 -0500

    agrego contacto

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../../img/img_components/Logo.jpg'
 import Navbar from '../Home/Navbar'
 import Footer from '../Home/Footer'
+import Contacto from '../Contacto/Contacto'
 import "../../styles/home.css"
 
 import { BrowserRouter as Router, Link } from 'react-router-dom';
@@ -25,7 +26,7 @@ function Home() {
                         <h1 className="display-5 fw-bold lh-1 mb-3">STRANGE_MUSIC</h1>
                         <p className="lead">La música expresa lo que no puede ser dicho y aquello sobre lo que es imposible
                             permanecer en silencio</p>
-                        <button type="button" className="button btn-lg px-4 me-md-2">Descargar Gratis</button>
+                        <button type="button" className="button btn-lg px-4 me-md-2 btn-descarga">Descargar Gratis</button>
                     </div>
                 </div>
             </div>
@@ -65,7 +66,7 @@ function Home() {
 
 
             {/* section descarga */}
-            <section className="pt-sm-3 mt-md-5 pt-sm-2 pt-md-5 touch1" id="descarga-con">
+            <section className="pt-sm-3 mt-md-5 pt-sm-2 pt-md-5">
                 <div className="container mt-sm-3 mt-md-5 ">
                     <div className="row align-items-center ">
                         <div className="col-md-5 col-lg-5  order-md-1 text-center z-index-2 cta-image " data-aos="fade-up" >
@@ -78,7 +79,7 @@ function Home() {
                         </div>
                         <div className="col-md-7 col-lg-7 text-center text-md-start">
                             <h1 className="display-5 fw-bold lh-sm mt-md-8  dark">
-                                Descarg Strange Music
+                                Descarga Strange Music
                             </h1>
                             <p className="my-4 fs-5 text-secondary">
                                 {" "}
@@ -116,51 +117,7 @@ function Home() {
             </section>
             {/* fin section descarga */}
 
-
-            <div class="container">
-                <footer class="py-5">
-                    <div class="row">
-                        <div class="col-6 col-md-2 mb-3">
-                            <h5>Section</h5>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-5 offset-md-1 mb-3">
-                            <form>
-                                <h5>Subscribe to our newsletter</h5>
-                                <p>Monthly digest of what's new and exciting from us.</p>
-                                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-                                    <label for="newsletter1" class="visually-hidden">Email address</label>
-                                    <input id="newsletter1" type="text" class="form-control" placeholder="Email address"></input>
-                                    <button type="button" className='button'>Subscribe</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 border-top">
-                        <p>&copy; STRANGE_MUSIC. Todos los derechos reservados 2023</p>
-                        <ul class="list-unstyled d-flex">
-                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
-                                <use xlink: href="#twitter" />
-                            </svg></a></li>
-                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
-                                <use xlink: href="#instagram" />
-                            </svg></a></li>
-                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
-                                <use xlink: href="#facebook" />
-                            </svg></a></li>
-                        </ul>
-                    </div>
-                </footer>
-            </div>
-
+            <Contacto/>
             <Footer />
         </>
 
