ccf4c41b32bc2f6b81a28959cd7865e136e26601
yonatan78 <yonatansuarez78@gmail.com>
2023-09-22 21:25:50 -0500

    agrego datos de limk

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -13,18 +13,9 @@ function Link() {
         {/* Contenido principal */}
         <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
           <div className="col-md-6 p-lg-5 mx-auto my-5">
-            <h1 className="display-3 fw-bold">Designed for engineers</h1>
-            <h3 className="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
-            <div className="d-flex gap-3 justify-content-center lead fw-normal">
-              <a className="icon-link" href="#">
-                Learn more
-                <svg className="bi"><use xlinkHref="#chevron-right" /></svg>
-              </a>
-              <a className="icon-link" href="#">
-                Buy
-                <svg className="bi"><use xlinkHref="#chevron-right" /></svg>
-              </a>
-            </div>
+            <h1 className="display-3 fw-bold">Nuevos lanzamientos</h1>
+            <h3 className="fw-normal text-muted mb-3">En esta sesion encontraras nuevos ritmos musicales</h3>
+
           </div>
           <div className="product-device shadow-sm d-none d-md-block"></div>
           <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
@@ -34,8 +25,8 @@ function Link() {
           <div className="col-md-6">
             <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
               <div className="my-3 py-3">
-                <h2 className="display-5">Another headline</h2>
-                <p className="lead">And an even wittier subheading.</p>
+                <h2 className="display-5">reggaeton</h2>
+                <p className="lead">El ritmo que mueve el mundo.</p>
               </div>
               <div className="bg-body-tertiary shadow-sm mx-auto" style={{ width: '80%', height: '300px', borderRadius: '21px 21px 0 0' }}></div>
             </div>
@@ -44,8 +35,8 @@ function Link() {
           <div className="col-md-6">
             <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
               <div className="my-3 py-3">
-                <h2 className="display-5">Another headline</h2>
-                <p className="lead">And an even wittier subheading.</p>
+                <h2 className="display-5">Salsa</h2>
+                <p className="lead">Donde la salsa se convierte en un viaje de pasión y alegría a través de la música.</p>
               </div>
               <div className="bg-body-tertiary shadow-sm mx-auto" style={{ width: '80%', height: '300px', borderRadius: '21px 21px 0 0' }}></div>
             </div>
@@ -71,7 +62,7 @@ function Link() {
         </footer>
       </div>
 
-      
+
       <Footer />
     </div>
   )
